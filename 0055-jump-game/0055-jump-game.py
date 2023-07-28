ba5dbf2e97a15d22aff13e397e8103baefd0d460
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-07-28 08:02:17 +0530

    Time: 396 ms (99.96%), Space: 17.6 MB (73.38%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,9 +1,8 @@
-class Solution {
-    public boolean canJump(int[] nums) {
-        int goodIndex = nums.length-1;
-        for(int i=nums.length-2;i>=0;i--){
-            if(i+nums[i] >= goodIndex) goodIndex = i;
-        }
-        return goodIndex == 0;
-    }
-}
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goodIndex = len(nums)-1
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i]+i >= goodIndex:
+                goodIndex = i
+        
+        return goodIndex == 0
