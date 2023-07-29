4ecfdd7132919ce4abbc6f30c521afc058d81487
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-07-29 15:32:04 +0530

    Time: 1 ms (99.20%), Space: 44.6 MB (58.70%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,15 +1,13 @@
 class Solution {
     public int jump(int[] nums) {
-        int count = 0;
-        int current_jump = 0;
-        int last_jump = 0;
-        for(int i = 0; i < nums.length-1; i++){
-            current_jump = Math.max(current_jump , i+nums[i]);
-            if(i == last_jump){
-                count++;
-                last_jump = current_jump;
-            }
-        }
-        return count;    
+      int jumps = 0, currEnd = 0, currFarthest = 0;
+      for(int i = 0; i < nums.length - 1; i ++) {
+          currFarthest = Math.max(currFarthest, i + nums[i]);
+          if(i == currEnd) {
+              currEnd = currFarthest;
+              jumps ++;
+          }
+      }  
+      return jumps;
     }
 }
